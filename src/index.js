5ddf25677e7afee119c95754d6acbb1009350b1f
Rob Scanlon <robscanlon@gmail.com>
2018-02-05 15:11:45 -0500

    Remove and clear out existing service workers.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+// Do not use service workers because they cause unexpected behavior and we
+// are not interested in offline modes currently. Unregister to clear out
+// previous registered service workers for people that have already visited
+// the app.
+serviceWorker.unregister();
