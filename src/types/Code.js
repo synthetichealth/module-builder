877c4fcf164e24ac572f3805a2ea320517d37074
Carlton Duffett <carlton.duffett@gmail.com>
2018-04-14 12:40:13 -0700

    Add NUBC, DICOM-DCM, and DICOM-SOP code options

--- src/types/Code.js
+++ src/types/Code.js
@@ -1,6 +1,6 @@
 // @flow
 export type Code = {
-  system: "SNOMED-CT" | "RxNorm" | "LOINC",
+  system: "SNOMED-CT" | "RxNorm" | "LOINC" | "NUBC" | "DICOM-DCM" | "DICOM-SOP",
   code: string,
   display: string
 }
