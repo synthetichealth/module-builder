9f2825d124a936ab097861b1ae0ab5776e90839d
Rob Scanlon <robscanlon@gmail.com>
2018-05-22 09:06:09 -0400

    Reorder conditional transitions.

--- src/components/editor/ComplexTransition.js
+++ src/components/editor/ComplexTransition.js
@@ -21,6 +21,68 @@ type Props = {
 }
 
 class ComplexTransition extends Component<Props> {
+
+  swapItems(indexTo,indexFrom) {
+    if(indexTo >= 0 && indexTo < this.props.transition.transition.length){
+      let oldEntry = {};
+      let newEntry = {};
+
+      if(this.props.transition.transition[indexTo].condition){
+        oldEntry.condition = _.clone(this.props.transition.transition[indexTo].condition)
+      }
+      if(this.props.transition.transition[indexTo].distributions){
+        oldEntry.distributions = _.clone(this.props.transition.transition[indexTo].distributions.map((d) => ({distribution: d.distribution, transition: d.to})))
+      }
+      if(this.props.transition.transition[indexTo].transition.to){
+        oldEntry.transition = this.props.transition[indexTo].transition.to
+      }
+
+      if(this.props.transition.transition[indexFrom].condition){
+        newEntry.condition = _.clone(this.props.transition.transition[indexFrom].condition)
+      }
+      if(this.props.transition.transition[indexFrom].distributions){
+        newEntry.distributions = _.clone(this.props.transition.transition[indexFrom].distributions.map((d) => ({distribution: d.distribution, transition: d.to})))
+      }
+      if(this.props.transition.transition[indexFrom].transition.to){
+        newEntry.transition = this.props.transition[indexFrom].transition.to
+      }
+
+      return () => {
+        this.props.onChange(indexTo)({val: {id: newEntry}});
+        this.props.onChange(indexFrom)({val: {id: oldEntry}});
+      }
+    }
+
+    return () => {};
+  }
+
+  renderUpDown(index: number) {
+  
+    if(this.props.transition && this.props.transition.transition){
+      if(index < this.props.transition.transition.length - 1 && index > 0){
+        return <span>
+            <a className="editable-text delete-button" onClick={this.swapItems(index-1, index)}>move up</a> | 
+            <a className="editable-text delete-button" onClick={this.swapItems(index, index+1)}>move down</a>
+          </span>
+      } else if(index < this.props.transition.transition.length - 1){
+        return <a className="editable-text delete-button" onClick={this.swapItems(index, index+1)}>move down</a>
+      } else if(index > 0){
+        return <a className="editable-text delete-button" onClick={this.swapItems(index-1, index)}>move up</a>
+      }
+    }
+  }
+
+  renderIf(index){
+    if(!this.props.transition.transition[index].condition){
+      return 'Else:'
+    }
+    if(index === 0){
+      return <span>If:</span>
+    }else{
+      return <span>Else If:</span>
+    }
+  }
+
   render() {
     let currentValue = [];
     if (this.props.transition) {
@@ -37,16 +99,20 @@ class ComplexTransition extends Component<Props> {
         {currentValue.map((t, i) => {
           return <div className='transition-option' key={i}>
             <label>
-              If: <ConditionalEditor {...this.props} conditional={t.condition} onChange={this.props.onChange(`${i}.condition`)}/>
+            {this.renderIf(i)} <ConditionalEditor {...this.props} conditional={t.condition} onChange={this.props.onChange(`${i}.condition`)}/>
             </label>
+            <br/>
             <label>
               <DistributedTransitionEditor transition={{transition: t.distributions}} options={states} onChange={this.props.onChange(`[${i}].distributions`)} />
             </label>
+            <br/>
+            {this.renderUpDown(i)}
             <br/>
             <a className='editable-text delete-button' onClick={() => this.props.onChange(`[${i}]`)({val: {id: null}})}>remove</a>
 
           </div>
         })}
+
         <a className='editable-text add-button' onClick={() => this.props.onChange(`[${currentValue.length}]`)({val: {id: getTemplate('Transition.Complex[0]')}})}>+</a>
 
       </label>
--- src/components/editor/ConditionalTransition.js
+++ src/components/editor/ConditionalTransition.js
@@ -18,6 +18,52 @@ type Props = {
 }
 
 class ConditionalTransition extends Component<Props> {
+
+  swapItems(indexTo,indexFrom) {
+    if(indexTo >= 0 && indexTo < this.props.transition.transition.length){
+
+      let oldEntry = {
+        transition: this.props.transition.transition[indexTo].to,
+        condition: _.clone(this.props.transition.transition[indexTo].condition)
+      };
+
+      let newEntry = {
+        transition: this.props.transition.transition[indexFrom].to,
+        condition: _.clone(this.props.transition.transition[indexFrom].condition)
+      }
+          
+      return () => {
+        this.props.onChange(indexTo)({val: {id: newEntry}});
+        this.props.onChange(indexFrom)({val: {id: oldEntry}});
+      }
+    }
+
+    return () => {};
+  }
+
+  renderUpDown(index: number) {
+  
+    if(this.props.transition && this.props.transition.transition){
+      if(index < this.props.transition.transition.length - 1 && index > 0){
+        return <span>
+            <a className="editable-text delete-button" onClick={this.swapItems(index-1, index)}>move up</a> | 
+            <a className="editable-text delete-button" onClick={this.swapItems(index, index+1)}>move down</a>
+          </span>
+      } else if(index < this.props.transition.transition.length - 1){
+        return <a className="editable-text delete-button" onClick={this.swapItems(index, index+1)}>move down</a>
+      } else if(index > 0){
+        return <a className="editable-text delete-button" onClick={this.swapItems(index-1, index)}>move up</a>
+      }
+    }
+  }
+  renderIf(index){
+    if(index === 0){
+      return <span>If:</span>
+    }else{
+      return <span>Else If:</span>
+    }
+  }
+
   render() {
     let currentValue = [];
     if (this.props.transition) {
@@ -26,6 +72,7 @@ class ConditionalTransition extends Component<Props> {
     if(!this.props.transition) {
       return null;
     }
+
     return (
       <label>
         Conditional Transition To:
@@ -33,11 +80,13 @@ class ConditionalTransition extends Component<Props> {
           let options = this.props.options.map((s) => {return {id: s.name, text: s.name}});
           return <div key={i} className='transition-option'>
             <label>
-              If: <ConditionalEditor {...this.props} conditional={t.condition} onChange={this.props.onChange(`${i}.condition`)}/>
+              {this.renderIf(i)} <ConditionalEditor {...this.props} conditional={t.condition} onChange={this.props.onChange(`${i}.condition`)}/>
             </label>
             <label>
               Transition To: <RIESelect className='editable-text' propName='to' value={{id:t.to, text:t.to}} change={this.props.onChange(`${i}.transition`)} options={options} />
             </label>
+            <br/>
+            {this.renderUpDown(i)}
             <br/>
             <a className="editable-text delete-button" onClick={() => this.props.onChange(`[${i}]`)({val: {id: null}})}>remove</a>
 
